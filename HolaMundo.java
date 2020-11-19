edcf5f8b66d54fdda9bc33e61013df2f6df9be63
pabloM022 <73890342+pabloM022@users.noreply.github.com>
2020-11-19 18:57:24 -0300

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,6 @@
 public class HolaMundo{
   public static void main(String[] args){
     System.out.println("Hola Mundo");
-    System.out.println("Arturo Avendaño");
+    System.out.println("Pablo Moretti");
     }
   }
